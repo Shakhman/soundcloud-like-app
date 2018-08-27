142cf5191c8df78fea289df7a868f0d72718a0f3
Shakhman <ooox1991@gmail.com>
2018-08-27 16:06:35 +0300

    initialize app

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+
+  componentDidMount() {
+    console.log('object');
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
